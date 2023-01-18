922e7e075b34964191cfbb6efe351795ce515ce8
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-18 02:28:48 +0200

    build: videodetails

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCirlcle } from "@mui/icons-material";
+import { fetchFromApi } from "./../utils/fetchdata";
 
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  const [VideoDetail, setVideoDetail] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
